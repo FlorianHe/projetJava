0da435170bcd35de0adcc48a41eef19277402b40
FlorianHe <florian.helaine@epitech.eu>
2023-06-19 14:48:22 +0200

    Exercice wilder

--- Wilders/wilders.java
+++ Wilders/wilders.java
@@ -32,7 +32,7 @@ class Wilder {
     }
 }
 
-public class Classroom {
+class Classroom {
     public static void main(String[] args) {
         Wilder wilder1 = new Wilder("Jean-Claude", true);
         Wilder wilder2 = new Wilder("Henri", false);
